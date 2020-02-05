38774006905e577a496c9997062a7ba7d3a5b6e4
Robby R <46580110+danke7127@users.noreply.github.com>
2020-02-04 19:02:58 -0500

    Update ControlPanel.java

--- src/main/java/org/longmetal/ControlPanel.java
+++ src/main/java/org/longmetal/ControlPanel.java
@@ -1,3 +1,37 @@
+package org.longmetal;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import com.pheonix.first.wpilibj.CANTalonSRX;
-import edu.wpi.first.wpilibj.ColorSensorV3;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.ColorSensorV3;
+
+public class ControlPanel {
+    private TalonSRX tSRX;
+    private DoubleSolenoid dSolenoid;
+    private ColorSensorV3 csensorV3;
+
+    private static boolean enabled = true;
+    private boolean initialized = false;
+
+    public ControlPanel(boolean setEnabled)
+    {
+        enabled = setEnabled;
+        if (enabled) 
+        {
+            init();
+        } 
+        else 
+        {
+            System.out.println("[WARN]\tShooter wasn't enabled on startup. You must call init() on it later to use it.");
+        }
+    }
+
+    public void init() 
+    {
+        //csensorV3 = new ColorSensorV3
+        //tSRX = new TalonSRX(Constants.kP_CONTROLPANEL);
+        //Note colorsensor has its own port but idk how to access it.
+        initialized = true;
+    }
+
+}
+
+
